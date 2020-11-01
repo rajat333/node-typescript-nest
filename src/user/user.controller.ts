9945946687ff9767062b18afb89b360a04f3cb75
Rajat khurana <Rajat khuranarajatkhurana@gmail.com>
2020-11-02 02:01:01 +0530

    DTO & method

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,19 @@
 import { Controller } from '@nestjs/common';
+import { CreateUserDTO } from './dto/create-user.dto';
+import { UserService } from './user.service';
+import { Get, Res, HttpStatus, Post, Body, Put, Query, NotFoundException, Delete, Param } from '@nestjs/common';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+    
+    constructor( private userService: UserService){}
+
+    @Post('/create')
+    async addCustomer(@Res() res, @Body() createUserDTO: CreateUserDTO) {
+        const customer = await this.userService.createUser(createUserDTO);
+        return res.status(HttpStatus.OK).json({
+            message: "User has been created successfully",
+            customer
+        })
+    }
+}
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from '@nestjs/common';
-
+import {  CreateUserDTO  } from './dto/create-user.dto';
 @Injectable()
-export class UserService {}
+export class UserService {
+
+    constructor(){}
+
+    async createUser(createUserDTO: CreateUserDTO): Promise<any> {
+        // const newUserr = await this.customerModel(createCustomerDTO);
+        // return newCustomer.save();
+    }
+}
